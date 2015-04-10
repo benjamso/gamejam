164dc24a3325947c283eea5c45c3f234f6f36ead
dosjos <j.skotterud@gmail.com>
2015-04-10 21:12:59 +0200

    Oppdatert HUD og gitignore

---
+++ FloaterWorld/Assets/Scripts/IslandTimer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IslandTimer : MonoBehaviour {
+	public float TimeLeft;
+	public float StartTime = 50.0f;
+	public float MaxTime;
+
+	public Slider IslandLeft;
+	public Slider IslandRigth;
+
+
+
+	// Use this for initialization
+	void Start () {
+		TimeLeft = StartTime;
+		MaxTime = StartTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		TimeLeft -= Time.deltaTime;
+
+		float remaining =  (TimeLeft / MaxTime) * 100;
+	
+		IslandLeft.value = remaining;
+		IslandRigth.value = remaining;
+
+		if (TimeLeft <= 0) {
+			//GameOver
+		}
+	}
+
+	public void UpdateTimeLeft(float plussTime){
+		TimeLeft += plussTime;
+
+		if (TimeLeft > MaxTime) {
+			MaxTime = TimeLeft;
+		}
+		float remaining =  (TimeLeft / MaxTime) * 100;
+		
+		IslandLeft.value = remaining;
+		IslandRigth.value = remaining;
+	}
+
+}
