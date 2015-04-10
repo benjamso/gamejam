d75516a0465f1e72033424622242e862218336f5
Martin Folkeseth <mafo@netlight.com>
2015-04-10 23:39:50 +0200

    Player collision detection with mob

--- FloaterWorld/Assets/Scripts/Player.cs
+++ FloaterWorld/Assets/Scripts/Player.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
+	public Text HUDText;
+
 
 	public int HP = 5;
 
 	// Use this for initialization
 	void Start () {
-	
+		HUDText.text = "HP: " + HP;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		HUDText.text = "HP: " + HP;
 	}
 }
