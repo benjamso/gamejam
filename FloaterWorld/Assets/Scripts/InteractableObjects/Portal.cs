2a5ec2ecb87e8f5541f05caaa87457896485e9a6
dosjos <j.skotterud@gmail.com>
2015-04-11 22:09:56 +0200

    La til portal som returnerer spilleren hjem

---
+++ FloaterWorld/Assets/Scripts/InteractableObjects/Portal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+	public bool CanTeleport;
+	public GameObject Player;
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		CanTeleport = true;
+	}
+	
+	void OnTriggerExit2D(Collider2D other){
+		CanTeleport = false;
+	}
+
+
+	void Update(){
+
+
+		if(CanTeleport && Input.GetKeyDown(KeyCode.E)){
+			Player.transform.position = new Vector3(0.96f,1.33f,0f);
+		}
+	}
+
+}
