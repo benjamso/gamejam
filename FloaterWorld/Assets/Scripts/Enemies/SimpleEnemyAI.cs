d58c478f74c935ada1b2b831602822b898b3ddf2
dosjos <j.skotterud@gmail.com>
2015-04-11 03:06:28 +0200

    Fullført en enkel dør, lagt til i prefab

--- FloaterWorld/Assets/Scripts/Enemies/SimpleEnemyAI.cs
+++ FloaterWorld/Assets/Scripts/Enemies/SimpleEnemyAI.cs
@@ -4,27 +4,20 @@ using System.Collections;
 public class SimpleEnemyAI : MonoBehaviour {
 
 	public float Speed;
-	public int direction = 1;
 	public float moveLength;
-	private float increment;
+	public int direction = -1;
 	private float initialX;
-
-
+	
 	private Rigidbody2D body;
-	// Use this for initialization
 	void Start () {
-		body = GetComponent<Rigidbody2D>();//.velocity = transform.right * Speed;
+		body = GetComponent<Rigidbody2D>();
 		initialX = body.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		increment += (Speed / 100) * direction;
-
 		Vector3 pos = transform.position;
-		pos.x = Mathf.PingPong(Time.time*2, moveLength) + initialX;
+		pos.x = (Mathf.PingPong(Time.time*2, moveLength) * direction) + initialX;
 		transform.position = pos;
-
 	}
 }
---
+++ FloaterWorld/Assets/Scripts/InteractableObjects/RisingDoor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RisingDoor : MonoBehaviour {
+
+
+	public float speed = 1.0F;
+	private float startTime;
+	private float journeyLength;
+	private bool open = false;
+	public bool active = true;
+	public RisingDoor otherTrigger;
+
+	public GameObject Door;
+	// Use this for initialization
+	void Start () {
+		journeyLength = Vector3.Distance(Door.transform.position, Door.transform.position);
+		active = true;
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("trig " + other.tag);
+		if (active && other.tag == "Player") {
+			startTime = Time.time;
+			open = true;
+
+			Door.transform.position = Vector3.Lerp (Door.transform.position, 
+			                                        new Vector3(Door.transform.position.x,
+													            Door.transform.position.y + 30,
+													            Door.transform.position.z)
+			                                        , Time.deltaTime * 2);
+			active = false;
+			otherTrigger.active = false;
+		}
+	}
+}
