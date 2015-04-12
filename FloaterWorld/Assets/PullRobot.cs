7cea449830f8eae86103cc8f982de9cba9fa2159
dosjos <j.skotterud@gmail.com>
2015-04-12 07:06:08 +0200

    Lagde trigger som styarter rotering av verden, og startet på å tiltrekke roboten tilmagneter

---
+++ FloaterWorld/Assets/PullRobot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PullRobot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+//public float pullRadius = 2;
+//    public float pullForce = 1;
+ 
+//     public void FixedUpdate() {
+ //        foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius) {
+ //            // calculate direction from target to me
+ //            Vector3 forceDirection = transform.position - collider.transform.position;
+ 
+             // apply force on target towards me
+//             collider.rigidbody.AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+//         }
+//     }
+// }
+}
