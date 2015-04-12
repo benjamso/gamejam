9be83d3f78cf084e73b4b74a15d767d87a9a7853
mei053 <mads_eg@hotmail.com>
2015-04-12 13:30:39 +0200

    merge and floating island
    Conflicts:
    	FloaterWorld/Assets/Images/Sprites/Floating_Island_Medium.png.meta
    	FloaterWorld/FloaterWorld-csharp.sln
    	FloaterWorld/FloaterWorld.sln
    	FloaterWorld/FloaterWorld.userprefs

---
+++ FloaterWorld/Assets/Scripts/Island/Tmp_Floating_Island.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tmp_Floating_Island : MonoBehaviour {
+
+	private bool playerTouchesFloor;
+
+	void Start () {
+		playerTouchesFloor = false;
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		playerTouchesFloor = true;
+		
+	}
+	
+	void OnTriggerExit2D(Collider2D other){
+		playerTouchesFloor = false;
+	}
+}
